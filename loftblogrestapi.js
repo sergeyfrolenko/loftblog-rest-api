b2a9977e85b8e0e90d3f3b0c66ef09445c6ba7cf
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-10-13 15:29:55 +0300

    begin database mongodb

--- loftblogrestapi.js
+++ loftblogrestapi.js
@@ -2,8 +2,14 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-const auto = [
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+let db
+
+let auto = [
   {
     name: 'BMW',
     foundation: 1916
@@ -21,7 +27,52 @@ const auto = [
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+app.get('/auto', (req, res) => {
+  res.send(auto)
+})
+app.get('/auto/:name', (req, res) => {
+  // console.log(req.params)
+  const getModel = auto.find((model) => {
+    return model.name === req.params.name
+  })
+  res.send(getModel ? "true" : "false")
+})
 
-app.listen(7000, () => {
-  console.log('Server has started on 7000 port')
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('post request is ')
+})
+app.post('/auto', (req, res) => {
+  const model = {
+    name: req.body.name,
+    foundation: req.body.foundation
+  }
+  auto.push(model)
+  res.send(auto)
+})
+
+app.put('/auto/:name', (req, res) => {
+  const getIndex = auto.findIndex((model) => {
+    return model.name === req.params.name.toUpperCase()
+  })
+  auto[getIndex].foundation = req.body.foundation
+  res.sendStatus(200)
+})
+
+app.delete('/auto/:name', (req, res) => {
+  auto = auto.filter((model) => {
+    return model.name !== req.params.name.toUpperCase()
+  })
+  res.sendStatus(200)
+  // res.send(auto)
+})
+
+
+
+MongoClient.connect('mongodb://localhost:27017/myapi', { useUnifiedTopology: true }, (err, database) => {
+  if (err) console.log(err)
+  db = database
+  app.listen(7000, () => {
+    console.log('Server has started on 7000 port')
+  })
 })
